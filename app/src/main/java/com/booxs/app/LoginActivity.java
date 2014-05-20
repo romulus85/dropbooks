dde6c51b026b21aaae5b317b021b29e81f4cee5c
romulus85 <djlargo50@gmail.com>
2014-05-20 22:07:19 +0200

    Isolated login activity.

--- app/src/main/java/com/booxs/app/LoginActivity.java
+++ app/src/main/java/com/booxs/app/LoginActivity.java
@@ -1,45 +1,83 @@
 package com.booxs.app;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.Toast;
 
+import com.dropbox.client2.DropboxAPI;
 import com.dropbox.client2.android.AndroidAuthSession;
+import com.dropbox.client2.android.AuthActivity;
 import com.dropbox.client2.session.AccessTokenPair;
 import com.dropbox.client2.session.AppKeyPair;
 import com.dropbox.client2.session.Session;
 
 
-public class MainActivity extends Activity {
+public class LoginActivity extends Activity {
 
-    final static private String APP_KEY = "6xhxpt0vnbqy2vm";
-    final static private String APP_SECRET = "1ew6rczb0095v60";
-    //the app can see al elements in dropbox not only the ones this app creates
-    final static private Session.AccessType ACCESS_TYPE = Session.AccessType.DROPBOX;
-    private static final String TAG = "MainActivity";
+
     // shared preferences to store credentials
     final static private String ACCOUNT_PREFS_NAME = "credentials_pref";
     final static private String ACCESS_KEY_NAME = "ACCESS_KEY";
     final static private String ACCESS_SECRET_NAME = "ACCESS_SECRET";
 
+    final static private String APP_KEY = "6xhxpt0vnbqy2vm";
+    final static private String APP_SECRET = "1ew6rczb0095v60";
+    //the app can see al elements in dropbox not only the ones this app creates
+    final static private Session.AccessType ACCESS_TYPE = Session.AccessType.DROPBOX;
+    private static final String TAG = "BooxsApplication";
+
+    //object to access and manage Dropbox api
+    public static DropboxAPI<AndroidAuthSession> mDBApi;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        Log.d(TAG, "onCreate");
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
 
-        setContentView(R.layout.activity_main);
+        // We create a new AuthSession so that we can use the Dropbox API.
+        AndroidAuthSession session = buildSession();
+        mDBApi = new DropboxAPI<AndroidAuthSession>(session);
 
-        if (savedInstanceState == null) {
-            Log.d(TAG, "loading fragment in mainactivity");
-            getFragmentManager().beginTransaction()
-                    .add(R.id.container, new EbookFragment())
-                    .commit();
+        checkAppKeySetup();
+
+        //if not liked then start oauth process
+        if (mDBApi.getSession().isLinked()) {
+            showMainActivity();
+        } else {
+            AppKeyPair appKeys = new AppKeyPair(APP_KEY, APP_SECRET);
+            mDBApi = new DropboxAPI<AndroidAuthSession>(session);
+            mDBApi.getSession().startOAuth2Authentication(LoginActivity.this);
         }
+
     }
 
+    @Override
     protected void onResume() {
+        Log.d(TAG, "onResume: isLinked = " + mDBApi.getSession().isLinked());
+        AndroidAuthSession session = mDBApi.getSession();
+        if (mDBApi.getSession().authenticationSuccessful()) {
+            try {
+                // Required to complete auth, sets the access token on the session
+                mDBApi.getSession().finishAuthentication();
+                storeAuth(session);
+                showMainActivity();
+            } catch (IllegalStateException e) {
+                Log.i("DbAuthLog", "Error authenticating", e);
+            }
+        }
         super.onResume();
+    }
+
+    private void showMainActivity() {
+        Log.d(TAG, "ShowMainActivity");
+        Intent launchMainActivityIntent = new Intent(this, MainActivity.class);
+        startActivity(launchMainActivityIntent);
+        finish();
     }
 
     /**
@@ -108,5 +146,31 @@ public class MainActivity extends Activity {
         AndroidAuthSession session = new AndroidAuthSession(appKeyPair);
         loadAuth(session);
         return session;
+    }
+
+    private void checkAppKeySetup() {
+        Log.d(TAG, "checkAppKeySetup");
+        // Check to make sure that we have a valid app key
+        if (APP_KEY.startsWith("CHANGE") ||
+                APP_SECRET.startsWith("CHANGE")) {
+            Toast.makeText(this, "You must apply for an app key and secret from developers.dropbox.com" +
+                    ", and add them to the DBRoulette ap before trying it.", Toast.LENGTH_LONG).show();
+            finish();
+            return;
+        }
+
+        // Check if the app has set up its manifest properly.
+        Intent testIntent = new Intent(Intent.ACTION_VIEW);
+        String scheme = "db-" + APP_KEY;
+        String uri = scheme + "://" + AuthActivity.AUTH_VERSION + "/test";
+        testIntent.setData(Uri.parse(uri));
+        PackageManager pm = getPackageManager();
+        if (0 == pm.queryIntentActivities(testIntent, 0).size()) {
+            Toast.makeText(this, "URL scheme in your app's " +
+                    "manifest is not set up correctly. You should have a " +
+                    "com.dropbox.client2.android.AuthActivity with the " +
+                    "scheme: " + scheme, Toast.LENGTH_LONG).show();
+            finish();
+        }
     }
 }
